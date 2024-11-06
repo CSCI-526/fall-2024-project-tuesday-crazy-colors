8fdedd9ddc93f7734e33fd71996b9ff3c14a5650
Atharva Swami <aswami@usc.edu>
2024-11-06 13:17:30 -0800

    added session time vs enemy kill count analytics metrics

--- Assets/Scripts/DeathAnalytics.cs
+++ Assets/Scripts/DeathAnalytics.cs
@@ -31,7 +31,7 @@ public class DeathAnalytics : MonoBehaviour
     }
 
 
-    public async void DeathLog(bool diedFromEnemy, bool diedFromColor, bool diedFromPlatform)
+    public async void DeathLog(bool diedFromEnemy, bool diedFromColor, bool diedFromPlatform, int enemyKillCount, string sessionTime)
     {
         if (!canLogDeath) return; // Prevent logging if not allowed
 
@@ -50,7 +50,7 @@ public class DeathAnalytics : MonoBehaviour
 
         Debug.Log($"Death Analytics - Enemy: {deathEnemy}, Color: {deathColor}, Platform: {deathPlatform}");
 
-        await Post(sessionID.ToString(), deathColor.ToString(), deathEnemy.ToString(), deathPlatform.ToString());
+        await Post(sessionID.ToString(), deathColor.ToString(), deathEnemy.ToString(), deathPlatform.ToString(), enemyKillCount.ToString(), sessionTime);
 
         // Start cooldown
         await Task.Delay(TimeSpan.FromSeconds(cooldownTime));
@@ -75,19 +75,23 @@ public class DeathAnalytics : MonoBehaviour
 
     //     Debug.Log($"Death Analytics - Enemy: {deathEnemy}, Color: {deathColor}, Platform: {deathPlatform}");
 
-    //     Post(sessionID.ToString(), deathColor.ToString(), deathEnemy.ToString(), deathPlatform.ToString());
+    //     Post(sessionID.ToString(), deathColor.ToString(), deathEnemy.ToString(), deathPlatform.ToString(), enemyKillCount.ToString(), sessionTime);
     // }
 
-    private async Task Post(string sessionID, string deathColor, string deathEnemy, string deathPlatform)
+    private async Task Post(string sessionID, string deathColor, string deathEnemy, string deathPlatform, string enemyKillCount, string sessionTime)
     {
+        Debug.Log($"Enemies Killed: {enemyKillCount}");
+        Debug.Log($"Session Time: {sessionTime}");
 
         string url = "https://docs.google.com/forms/u/1/d/e/1FAIpQLSfJZj4Q5X-65JhUFUVRbG9Ns0kfnFqNnTFiF_iUoSGThT2T5A/formResponse";
 
         WWWForm form = new WWWForm();
-        form.AddField("entry.1692969652", sessionID);
+        form.AddField("entry.1692969652", sessionID.ToString());
         form.AddField("entry.1603297951", deathColor);
         form.AddField("entry.1751409824", deathEnemy);
         form.AddField("entry.1151262886", deathPlatform);
+        form.AddField("entry.1366656562", enemyKillCount.ToString());
+        form.AddField("entry.2061567090", sessionTime.ToString());
 
         using (UnityWebRequest www = UnityWebRequest.Post(url, form))
         {
@@ -104,7 +108,7 @@ public class DeathAnalytics : MonoBehaviour
                 {
                     Debug.Log("Received 429 error. Retrying after delay...");
                     await Task.Delay(10000); // Wait for 10 seconds
-                    await Post(sessionID, deathColor, deathEnemy, deathPlatform); // Retry sending
+                    await Post(sessionID, deathColor, deathEnemy, deathPlatform, enemyKillCount, sessionTime); // Retry sending
                 }
                 else
                 {
