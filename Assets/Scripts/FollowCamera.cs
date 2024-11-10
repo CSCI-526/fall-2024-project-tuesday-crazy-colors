70b1ea01cdeab5ebcd4618efa78839ef2d0bcc02
Pooja Kaulgud <pooja.kaulgud@somaiya.edu>
2024-11-10 03:52:59 -0800

    Cloud jittering fixed

--- Assets/Scripts/FollowCamera.cs
+++ Assets/Scripts/FollowCamera.cs
@@ -7,7 +7,8 @@ public class FollowCamera : MonoBehaviour
     public Transform cameraTransform; // Assign the camera in the Inspector
     public Vector3 offset; // Set the initial offset distance in the Inspector
     public float followSpeed = 1.0f; // Speed at which the clouds follow the camera
-
+    private Vector3 velocity = Vector3.zero;
+    public float smoothTime = 0.8f;
     void LateUpdate()
     {
         if (cameraTransform != null)
@@ -16,7 +17,8 @@ public class FollowCamera : MonoBehaviour
             Vector3 targetPosition = cameraTransform.position + offset;
 
             // Move the cloud smoothly to follow the camera
-            transform.position = Vector3.Lerp(transform.position, targetPosition, followSpeed * Time.deltaTime);
+            // transform.position = Vector3.Lerp(transform.position, targetPosition, followSpeed * Time.deltaTime);
+            transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
         }
     }
 }
