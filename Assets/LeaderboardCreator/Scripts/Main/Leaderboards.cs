30327a7d4ccc0868ccc9be7d1a765d4c85107582
tanvik22 <tkhasgiw@usc.edu>
2024-11-27 12:52:56 -0800

    Leaderboard Complete

---
+++ Assets/LeaderboardCreator/Scripts/Main/Leaderboards.cs
@@ -0,0 +1,8 @@
+namespace Dan.Main
+{
+    public static class Leaderboards
+    {
+        public static LeaderboardReference DemoSceneLeaderboard = new LeaderboardReference("980324f10e2f03fc0fd6a38a70f88123919a6aed35a4aea86211ad58b68db9c4");
+        public static LeaderboardReference CC = new LeaderboardReference("8806e339b33f648fb502ee6fd7d0c89e954d9b3b2fb4edaf38253f00f512e2ae");
+    }
+}
